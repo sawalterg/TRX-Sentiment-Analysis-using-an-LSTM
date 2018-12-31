510438709878e0cc2c686475a6b08ab5368b349a
sawalterg <38083974+sawalterg@users.noreply.github.com>
2018-12-30 21:06:47 -0500

    Update get_sentiment_price_data.py

--- get_sentiment_price_data.py
+++ get_sentiment_price_data.py
@@ -1,60 +1,46 @@
-"""
-def dict_format(post):
-  #Helper function for converting PRAW objects to python dictionary
-    result = {}
-    for k, v in post.__dict__.items():
-        if k.startswith('_'):
-            continue
-        if k in {'author', 'subreddit'}:
-            result[k] = str(v)
-            continue
-        if v is None:
-            continue
-        result[k] = v
-    return result
+# Pull the crypto currency prices
 
-submissions = reddit.subreddit('Vechain').top(limit=500)
-
-# load into pandas
-subs = pd.DataFrame(dict_format(post) for post in submissions)
-
-# change the `created_utc` column to a datetime object
-subs['created_utc'] = pd.to_datetime(subs.created_utc, unit='s')
-
-
-
-subs['sentiment'] = subs['title'].apply(sent_calc)
-
-for time_period, group in subs.groupby(pd.Grouper(key='created_utc', freq='d')):
-    titles = group.title.values
-    # do something with titles...
-    print(time_period, len(group)"""
-    
-     
-          
-          
-# Pull the crypto currency prices using cmc API
-
-from cmc import coinmarketcap
-from datetime import datetime
-
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 
 
 # Select cryptocurrency 
+crypto = 'vechain'
+start_date, end_date = '20170101', '20181230'       
 
-crypto = 'VeChain'
-start_date, end_date = datetime(2016,1,1), datetime(2018,12,1)          
-
-vechain_df = coinmarketcap.getDataFor(crypto, start_date, end_date)
-
-
-#
-
-
-
-### Crate empty dictionary for storing all Reddit data
+url = 'https://coinmarketcap.com/currencies/{0}/historical-data/?start={1}&end={2}'.format(crypto, start_date, end_date)
+  
+content = requests.get(url).content
+soup = BeautifulSoup(content, 'html.parser')
+table = soup.find('table', {'class': 'table'})
 
 
+table_form = [[td.text.strip() for td in tr.findChildren('td')] 
+        for tr in table.findChildren('tr')]
+
+df_vet = pd.DataFrame(table_form)
+df_vet.drop(df_vet.index[0], inplace=True) # first row is empty
+df_vet[0] =  pd.to_datetime(df_vet[0]) # date
+for i in range(1,7):
+    df_vet[i] = pd.to_numeric(df_vet[i].str.replace(",","").str.replace("-","")) # some vol is missing and has -
+df_vet.columns = ['Date','Open','High','Low','Close','Volume','Market Cap']
+df_vet.set_index('Date',inplace=True)
+df_vet.sort_index(inplace=True)
+
+
+
+### Create empty dictionary for storing all Reddit data
+
+import praw
+
+from reddit_creds import *
+
+reddit = praw.Reddit(client_id = 'oCrzM31o8070bA',
+                     client_secret='kBrPF2ILOwtZVwLlUv-5ELHwRpE', 
+                     username = user_name_red,
+                     password= password_red,
+                     user_agent= 'anything')
 dict = { "title":[],
                 "subreddit":[],
                 "score":[], 
@@ -79,28 +65,47 @@ for submission in reddit.subreddit(crypto).top(limit=10000):
 
 reddit_df = pd.DataFrame(dict)
 
+from textblob import TextBlob
+
+# Change to datetime format
+
 reddit_df['created_utc'] = pd.to_datetime(reddit_df.created, unit='s')
 
-reddit_df[['polarity', 'subjectivity']] = reddit_df['title'].apply(lambda Text: pd.Series(textblob.TextBlob(Text).sentiment))
+# Use textblob's pretrained polarity method to determine the sentiment of title
+
+reddit_df[['polarity', 'subjectivity']] = reddit_df['title'].apply(lambda Text: pd.Series(TextBlob(Text).sentiment))
+
+# Retrieve relevent columns
 
 reddit_df_ex = reddit_df[['created_utc', 'polarity']]
 
+# Switch to index for resampling to a day
+
 reddit_df_ex = reddit_df_ex.set_index('created_utc')
+
+# Take the average sentiment of the day using the mean method
 
 grouped_df = reddit_df_ex.resample('D').mean()
 
+# Reset index
+
 grouped_df = grouped_df.reset_index()
 
-vechain_df = vechain_df.reset_index()
 
-import pandas as pd
+#grouped_df['created_utc'] = pd.to_datetime(grouped_df.created_utc, unit='s')
 
-merged_df = pd.merge(vechain_df['Open'], grouped_df, left_on = 'Date', right_on = 'created_utc', how = 'inner')
 
-merged_df = merged_df[['created_utc', 'vechain_Open', 'polarity' ]]
+# Create new index
 
+df_vet = df_vet.reset_index()
+
+
+
+merged_df = pd.merge(df_vet, grouped_df, left_on = 'Date', right_on = 'created_utc', how = 'inner')
+
+merged_df = merged_df[['Date', 'Open', 'polarity' ]]
+
+
+# Make final csv
 
 merged_df.to_csv('sent_price_file.csv', sep = ',')
-
-
-
