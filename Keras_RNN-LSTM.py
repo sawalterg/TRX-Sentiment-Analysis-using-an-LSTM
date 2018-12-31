cd639764e7d46fdecc785c68c7c15195c06f6698
sawalterg <38083974+sawalterg@users.noreply.github.com>
2018-12-31 18:50:41 -0500

    Update Keras_RNN-LSTM.py

--- Keras_RNN-LSTM.py
+++ Keras_RNN-LSTM.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from math import sqrt
+import tensorflow
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import mean_squared_error
@@ -10,100 +11,151 @@ from keras.layers import Dense
 from keras.layers import LSTM
 import seaborn as sns
 import numpy as np
+from collections import deque
+from sklearn.preprocessing import RobustScaler
+import random
+from sklearn.preprocessing import RobustScaler
 
 
 df = pd.read_csv('sent_price_file.csv', sep = ',')
 
-
-# Data Cleaning so there are no null values
-
-df['Open'].replace(0, np.nan, inplace = True)
-df['Open'].fillna(method ='ffill', inplace = True)
-
-"""df['polarity'].replace(0, np.nan, inplace = True)
-df['polarity'].fillna(method ='ffil', inplace = True)
-"""
+df.head()
+df.info()
+df.describe()
 
 
+df = df.drop(columns = 'Unnamed: 0') # Remove index column
+df = df.drop(columns = 'Volume')
+
+
+df = df.set_index('Date')
+
+df = df.dropna()
+
+
+# Plotting values
+
+_ = plt.plot(df.Close, color = 'blue')
+_ = plt.plot(df.polarity, color = 'red')
+_ = plt.legend()
+plt.show()
+
+# Checking for correlation between values
+
+
+corr = df.corr()
+f, ax = plt.subplots(figsize=(6, 3))
+plt.title("Variable Correlation Plot")
+sns.heatmap(corr, 
+            xticklabels=corr.columns.values,
+            yticklabels=corr.columns.values)
 
 # Scaling the dataset for the neural network
 
 
-# Create scaling object
 
-sc = MinMaxScaler(feature_range = (0,1))
-
-
-price = df['Open'].values.reshape(-1, 1).astype('float32')
-sent = df['polarity'].values.reshape(-1, 1).astype('float32')
-
-
-
-price_sc = sc.fit_transform(price)
-
-# Split data
-# Splitting Training Sheets to Test Sheets
-from sklearn.model_selection import train_test_split 
-train, test = train_test_split(price_sc, test_size = 0.25, random_state = 0) 
-
-
-# Create a function for making this dataset have lookback
-
-def look_back_df(df, look_back, sent):
-    x, y = [], []
-    for i in range(len(df) - look_back):
-        a = df[i:i + look_back, 0]
-        np.append(a, sent[i])
-        x.append(a)
-        y.append(df[i + look_back, 0])
-    print(len(y))
-    return np.array(x), np.array(y)
-
-
-# Look back dataset
+  
+    def finalize_df(df, test=True):
+        df_edited = df.copy()
+        steps = 60
     
-x_train, y_train = look_back_df(train, 1, sent[0:len(train)])
-x_test, y_test = look_back_df(test, 1, sent[len(train) : len(price_sc)])
+        sc = MinMaxScaler(feature_range = (0,1))
+        df = sc.fit_transform(df)
+    
+        seq_data = []  # this is a list that will CONTAIN the sequences
+        prev_days = deque(maxlen=steps)  # deque removes old values as new ones come in
+    
+        for i in df: 
+            prev_days.append([n for n in i[:-1]])  # do not include y
+    
+            # check for 60 days of previous data
+            if len(prev_days) == steps:
+                # seq data = [prev_days_data, target variable]
+                seq_data.append([np.array(prev_days), i[-1]])  
+        X = []
+        y = []
+    
+        for seq, target in seq_data:  # iterating though the data
+            X.append(seq)  # store the seqs for features
+            y.append(target)  # y is the target
+        
+        if test == True:
+            return np.array(X), y 
+        else:
+            return np.array(X), y, df_edited, sc
 
 
-# Reshape train data
+date = sorted(df.index.values)
+percentage = sorted(df.index.values)[-int(0.3*len(date))]
 
-x_train = np.reshape(x_train, (x_train.shape[0], 1, x_train.shape[1]))
-x_test = np.reshape(x_test, (x_test.shape[0], 1, x_test.shape[1]))
 
+
+validation_main_df = df[(df.index >= percentage)]
+main_df = df[(df.index < percentage)]
+
+
+
+x_train, y_train = finalize_df(main_df)
+x_test, y_test, df_final, sc_0 = finalize_df(validation_main_df, test=False)
 
 # Initiate neural network
 
 model = Sequential()
-model.add(LSTM(100, input_shape=(x_train.shape[1], x_train.shape[2])))
-model.add(Dense(1))
-model.compile(loss='mae', optimizer='adam')
-history = model.fit(x_train, y_train, epochs=100, batch_size=50, validation_data=(x_test, y_test), verbose=0, shuffle=False)
+model.add(LSTM(50, input_shape=(x_train.shape[1:]), dropout = 0.1, return_sequences = True))
+model.add(LSTM(50, dropout = 0.1))
+
+model.add(Dense(1, activation = 'tanh'))
+model.compile(loss='mse', optimizer='adam', metrics = ['mae', 'mse'])
+print(model.summary())
+history = model.fit(x_train, y_train, epochs=5, batch_size=100)
+
+score_train = model.evaluate(x_train, y_train, verbose = 0)
+
+score_test = model.evaluate(x_test, y_test, verbose = 0)
 
 
 # Plot the loss by epoch
 
-y_pred = model.predict(x_test)
 _ = plt.plot(history.history['loss'], label='train')
-_ = plt.plot(history.history['val_loss'], label='test')
 _ = plt.legend()
 plt.show()
 
+# Predict 
+
+y_pred = model.predict(x_test)
 
 # Revert back to normal value from the scaled value
+padding = np.zeros((38, 1))
+y_pred_result = pd.DataFrame(data=padding)
+y_pred_result['9'] = y_pred
+y_pred_inv = sc.inverse_transform(y_pred_result)
 
-y_pred_inv = scaler.inverse_transform(y_pred.reshape(-1,1))
-y_test_inv = sc.inverse_transform(y_test.reshape(-1,1))
+y_test_results = pd.DataFrame(data=padding)
+y_test_results['9'] = y_test
+y_test_inv = sc.inverse_transform(y_test_results)
 
 # check the root mean squared error
-
-rmse = sqrt(mean_squared_error(y_test_inv, y_pred_inv))
 
 
 
 # Plot y pred vs y actual
 
-_ = plt.plot(y_pred, label = 'Predicted Value')
-_ = plt.plot(y_test_inv, label = 'Actual')
+_ = plt.plot(y_test_inv[:,-1], label = 'Predicted Value')
+_ = plt.plot(y_pred_inv[:, -1], label = 'Actual')
+_ = plt.title('Predictions vs. Actual')
+_ = plt.ylabel('Closing Price (Unscaled)')
 _ = plt.legend()
 plt.show()
+
+
+
+
+
+
+model2 = Sequential()
+model2.add(LSTM(50, input_shape=(x_train.shape[1:]), dropout = 0.1, return_sequences = True))
+model2.add(LSTM(50, dropout = 0.1, return_sequences = True))
+model.add(Dense(1))
+model2.compile(loss='mse', optimizer='adam', metrics = ['mae', 'mse'])
+print(model2.summary())
+history2 = model2.fit(x_train, y_train, epochs=500, batch_size=50)
